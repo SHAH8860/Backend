e79d7a9fbf885b4b0ced332bc86126216cb0f1b8
niraj <kumarnirajshah@gmail.com>
2022-07-19 00:21:16 +0530

    init

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
+var bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: false }))
 const port = process.env.PORT||3000
 const connection=require('./connection')
 const router=require('./Routes/routes')
+app.use(bodyParser.json())
 app.use('/routes',router)
 app.get('/', (req, res) => {
   res.send('Hello World!')
