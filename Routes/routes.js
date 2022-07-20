a5a38fc2625f6da8418f92d34df9530f1710530b
SHAH8860 <kumarnirajshah@gmail.com>
2022-07-20 16:24:36 +0530

    init

--- Routes/routes.js
+++ Routes/routes.js
@@ -1,16 +1,15 @@
 
-var express = require('express')
-var router = express.Router()
-var connection=require('../connection')
+const express = require('express')
+const router = express.Router()
+const connection=require('../connection')
 router.post('/signup',(req,res)=>{
     let user=req.body
     console.log(req.body)
-     query="select email,password,role,status from person where email=?"
+    let query="select email,password,role,status from person where email=?"
     connection.query(query,[user.email],(err,result)=>{
         if(!err){
-            console.log("result=",result)
             if(result<=0){
-                 query='insert into person (name,contactnumber,email,password,status,role) values(?,?,?,?,"false","user")'
+                let query='insert into person (name,contactnumber,email,password,status,role) values(?,?,?,?,"false","user")'
                 connection.query(query,[user.name,user.contactnumber,user.email,user.password],(err,result)=>{
                     if(!err){
                         return res.status(200).json({message:'Succesful register'})
